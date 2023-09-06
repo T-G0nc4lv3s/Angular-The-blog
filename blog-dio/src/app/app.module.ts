a1411ed308e793faeed83bffb4aa696dfc3b05f0
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-06 16:13:19 -0300

    banner component

--- blog-dio/src/app/app.module.ts
+++ blog-dio/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
