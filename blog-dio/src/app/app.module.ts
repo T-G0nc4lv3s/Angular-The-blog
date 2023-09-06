7f0322e67ad2b0535a3d592440269f5452a96910
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-06 18:32:01 -0300

    big-card component

--- blog-dio/src/app/app.module.ts
+++ blog-dio/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     BannerComponent,
-    HomeComponent
+    HomeComponent,
+    BigCardComponent
   ],
   imports: [
     BrowserModule
