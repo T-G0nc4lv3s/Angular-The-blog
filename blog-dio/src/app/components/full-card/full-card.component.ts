40e2f13842c35bd5f3d877f3e54a3f9ccb9c392d
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-10 18:44:44 -0300

    full-card component

---
+++ blog-dio/src/app/components/full-card/full-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-full-card",
+  templateUrl: "./full-card.component.html",
+  styleUrls: ["./full-card.component.css"],
+})
+export class FullCardComponent {
+  imgUrl: String =
+    "https://fictionlit.com/wp-content/uploads/2023/08/Evil-Saitama.png";
+  cardTitle: String = "Ab excepturi consequatur eum obcaecati illo.";
+  description: String =
+    "Et obcaecati odit aut aliquid libero qui voluptate nemo eos consequatur nobis. Ut nesciunt labore vel molestiae soluta et doloremque magni aut consequatur nemo ea cumque magnam eos molestiae fugit ut earum consectetur. Aut similique enim eum officia earum non aspernatur eveniet sed modi reiciendis non galisum tenetur? Eum internos odio sed omnis impedit non perspiciatis distinctio ut incidunt illum qui repellat omnis qui cupiditate possimus sed omnis repudiandae. ";
+}
