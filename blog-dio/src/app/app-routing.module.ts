9db3ad2a6a19581c63e54915c820577559220396
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-10 12:13:29 -0300

    app-routing module

---
+++ blog-dio/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { ContentComponent } from "./pages/content/content.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "content",
+    component: ContentComponent,
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- blog-dio/src/app/app.module.ts
+++ blog-dio/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { MenuListComponent } from './components/menu-list/menu-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { MenuListComponent } from './components/menu-list/menu-list.component';
     HomeComponent,
     BigCardComponent,
     SmallCardComponent,
-    MenuListComponent
+    MenuListComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
