0d2c09973b53a4d88f668d9b9b6e6166e313b82b
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-11 11:49:54 -0300

    Data mock

---
+++ blog-dio/src/app/data/DataFake.ts
@@ -0,0 +1,75 @@
+import { BlogNews } from "./../domain/BlogNews";
+
+export const dataCollection: Array<BlogNews> = [];
+
+let obj1 = new BlogNews(
+  "1",
+  "https://encdn.ldmnq.com/faq/images/pt/c667984d-efe6-4316-adab-1932c26c1623.jpg",
+  "Ab excepturi consequatur eum obcaecati illo.",
+  "Et obcaecati odit aut aliquid libero qui voluptate nemo eos consequatur nobis. Ut nesciunt labore vel molestiae soluta et doloremque magni aut consequatur nemo ea cumque magnam eos molestiae fugit ut earum consectetur. Aut similique enim eum officia earum non aspernatur eveniet sed modi reiciendis non galisum tenetur? Eum internos odio sed omnis impedit non perspiciatis distinctio ut incidunt illum qui repellat omnis qui cupiditate possimus sed omnis repudiandae."
+);
+
+dataCollection.push(obj1);
+
+let obj2 = new BlogNews(
+  "2",
+  "https://i.pinimg.com/originals/67/da/44/67da44d3ba693713128ba3e55801981f.jpg",
+  "Est facere voluptas qui itaque blanditis .",
+  null
+);
+
+dataCollection.push(obj2);
+
+let obj3 = new BlogNews(
+  "3",
+  "https://sm.ign.com/t/ign_br/screenshot/default/ssa_4fzu.1280.jpg",
+  "Ut impedit cumque sed quam inventore. ",
+  null
+);
+
+dataCollection.push(obj3);
+
+let obj4 = new BlogNews(
+  "4",
+  "https://fictionlit.com/wp-content/uploads/2023/08/Evil-Saitama.png",
+  "Est consequatur laudantium ut. ",
+  null
+);
+
+dataCollection.push(obj4);
+
+let obj5 = new BlogNews(
+  "5",
+  "https://s.aficionados.com.br/imagens/esta-e-a-ordem-cronologica-certa-para-assistir-dragon-ball_f.jpg",
+  "Quo sint dolorem quo nostrum impedit. ",
+  null
+);
+
+dataCollection.push(obj5);
+
+let obj6 = new BlogNews(
+  "6",
+  "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/26079ab060401d8a76039e985d63a3e9.jpe",
+  "Eum temporibus tempore aut internos nihil.",
+  null
+);
+
+dataCollection.push(obj6);
+
+let obj7 = new BlogNews(
+  "7",
+  "https://miro.medium.com/v2/resize:fit:1400/0*qKksSvflsm_XHdI7.jpg",
+  "Ab iste blanditiis aut galisum omnis et iure adipisci. ",
+  null
+);
+
+dataCollection.push(obj7);
+
+let obj8 = new BlogNews(
+  "8",
+  "https://wallpapercg.com/download/saitama-8k-7680x4320-10470.jpg",
+  "Et odio placeat non aliquam quis.",
+  null
+);
+
+dataCollection.push(obj8);
