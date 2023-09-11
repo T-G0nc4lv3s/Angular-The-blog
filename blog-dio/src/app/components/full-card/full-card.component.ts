ae46960f816d82081175cd3147b7faa153bd7d2f
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-11 11:59:20 -0300

    full-card component receiving parameters dynamically

--- blog-dio/src/app/components/full-card/full-card.component.ts
+++ blog-dio/src/app/components/full-card/full-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-full-card",
@@ -6,9 +6,12 @@ import { Component } from "@angular/core";
   styleUrls: ["./full-card.component.css"],
 })
 export class FullCardComponent {
-  imgUrl: String =
-    "https://fictionlit.com/wp-content/uploads/2023/08/Evil-Saitama.png";
-  cardTitle: String = "Ab excepturi consequatur eum obcaecati illo.";
-  description: String =
-    "Et obcaecati odit aut aliquid libero qui voluptate nemo eos consequatur nobis. Ut nesciunt labore vel molestiae soluta et doloremque magni aut consequatur nemo ea cumque magnam eos molestiae fugit ut earum consectetur. Aut similique enim eum officia earum non aspernatur eveniet sed modi reiciendis non galisum tenetur? Eum internos odio sed omnis impedit non perspiciatis distinctio ut incidunt illum qui repellat omnis qui cupiditate possimus sed omnis repudiandae. ";
+  @Input()
+  id: String = "";
+  @Input()
+  imgUrl: String = "";
+  @Input()
+  cardTitle: String = "";
+  @Input()
+  description: String = "";
 }
